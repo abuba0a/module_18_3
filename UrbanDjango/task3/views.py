31a5da418a704f72d5a0d1bd209149914d4ab0f1
abuba0a <168736244+abuba0a@users.noreply.github.com>
2024-11-15 07:59:21 +0300

    Update views.py

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -4,12 +4,33 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'platform.html')
+    title = 'Play'
+    headline = 'Главная страница'
+    context = {
+        'title': title,
+        'headline': headline,
+    }
+    return render(request, 'platform.html', context)
 
 
 def game_page(request):
-    return render(request, 'games.html')
+    title = 'Games'
+    text = 'Игры'
+    pay = 'Купить'
+    context = {
+        'title': title,
+        'text': text,
+        'pay': pay,
+    }
+    return render(request, 'games.html', context)
 
 
 def cart_page(request):
-    return render(request, 'cart.html')
+    title = 'Cart'
+    text = 'Корзина для новых опций'
+    context = {
+        'title': title,
+        'text': text,
+    }
+    return render(request, 'cart.html', context)
+
